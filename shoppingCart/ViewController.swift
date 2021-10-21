39381dc36d20c92a55d751b8c95178cfa7bfd045
John <john.brucker23@d155.org>
2021-10-21 07:36:38 -0500

    Initial Commit

---
+++ shoppingCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingCart
+//
+//  Created by JOHN BRUCKER on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
